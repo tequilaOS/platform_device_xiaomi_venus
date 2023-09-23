c8e73651221cef96a6e70c7bbe031df359d795ce
Retypolkg <retypolkg2021@gmail.com>
2023-09-23 20:20:07 +0200

    venus: Time for tequila!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_venus.mk
+    $(LOCAL_DIR)/tequila_venus.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_venus-user \
-    lineage_venus-userdebug \
-    lineage_venus-eng
+    tequila_venus-user \
+    tequila_venus-userdebug \
+    tequila_venus-eng
--- tequila_venus.mk
+++ tequila_venus.mk
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from venus device
 $(call inherit-product, device/xiaomi/venus/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common tequila stuff.
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := venus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2011K2G
-PRODUCT_NAME := lineage_venus
+PRODUCT_NAME := tequila_venus
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
